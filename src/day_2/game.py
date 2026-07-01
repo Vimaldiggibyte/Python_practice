73f640b1b66c074da675a035265a5025f75ef8a3
VimalRajSt <vimalrajtemp@gmail.com>
2026-07-01 13:07:25 +0530

    Modify game.py

--- src/day_2/game.py
+++ src/day_2/game.py
@@ -1,10 +1,27 @@
-def check_guess(guess, secret_number):
+import random
+from utils import check_guess
 
-    if guess < secret_number:
-        return "low"
 
-    elif guess > secret_number:
-        return "high"
+def start_game():
+    secret_number = random.randint(1, 100)
 
-    else:
-        return "correct"
+    attempts = 0
+
+    print("\nI have selected a number between 1 and 100.")
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        result = check_guess(guess, secret_number)
+
+        if result == "low":
+            print("Too Low!")
+
+        elif result == "high":
+            print("Too High!")
+
+        else:
+            print("\nCongratulations!")
+            print(f"You guessed the number in {attempts} attempts.")
+            break
